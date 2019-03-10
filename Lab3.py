9c4d58369f43514a9f77db7a1c0590a6ea7f9e6c
BenCas <38674334+BenCas@users.noreply.github.com>
2019-03-10 10:28:12 -0700

    Add files via upload

---
+++ Lab3.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+
+class Cart(object):
+    def __init__(self, name, price):
+        self.name = []
+        self.price = []
+    
+class Product(object)
+    def _init_(self, name, price):
+        self.name = []
+        self.price = []
+
+      def addProduct(self, product)
+        self.product.append(product)
+
+Product = namedtuple("name", "Price")
+
+cart = Cart()
+cart.addProduct(Product("Orange", "4.99"))
+cart.addProduct(Product("Apple", "3.99"))
+
+subTotal = (cart)
+print “subtotal = $” +str(subtotal)
+
+salesTax = subtotal * 0.06
+print “sales tax = $” +str(salesTax)
+
+totalAmount = (Subtotal + SalesTax)
+
+print “Total = $” +str(totalAmount)
